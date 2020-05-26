fe19c35b59278519c260d04b09f4d9f44ef1aed4
tjacek <tjacek@agh.edu.pl>
2020-05-26 23:57:56 +0200

    soft voting

--- learn.py
+++ learn.py
@@ -7,22 +7,29 @@ def simple_exp(data):
     y_true,y_pred,names=train_model(data)
     return accuracy_score(y_true,y_pred)
 
-def train_model(data):
+def train_model(data, binary=True):
     if(type(data)==str):	
         data=feats.read(data)[0]
     data.norm()
     train,test=data.split()
     model=LogisticRegression(solver='liblinear')
+#    print(dir(model))
     model.fit(train.X,train.get_labels())
     y_true=test.get_labels()
-    y_pred=model.predict(test.X)
+    if(binary):
+        y_pred=model.predict(test.X)
+    else:
+        y_pred=model.predict_proba(test.X)
     return y_true,y_pred,data.info
 
-def ensemble_exp(in_path):
+def ensemble_exp(in_path,binary=True):
     datasets=feats.read(in_path)
-    results=[train_model(data_i) for data_i in datasets]
+    results=[train_model(data_i,binary) for data_i in datasets]
     y_true=results[0][0]
-    votes=np.array([to_one_hot(result_i[1]) for result_i in results])
+    if(binary):
+        votes=np.array([to_one_hot(result_i[1]) for result_i in results])
+    else:
+        votes=np.array([result_i[1] for result_i in results])
     votes=np.sum(votes,axis=0)
     y_pred=[np.argmax(vote_i) for vote_i in votes]
     print(accuracy_score(y_true,y_pred))
@@ -37,4 +44,4 @@ def to_one_hot(y):
     return np.array(one_hot)
 
 if __name__=="__main__":
-    ensemble_exp("../ens5/sim/feats")
+    ensemble_exp("../ens5/sim/feats",False)
