847a2e926c5dd23ffcc4e4ed9f82cc015f5c6f0d
tjacek <tjacek@agh.edu.pl>
2020-06-25 17:12:43 +0200

    get data

--- acc_curve.py
+++ acc_curve.py
@@ -1,15 +1,23 @@
 import numpy as np
-import tools,clf,learn.report
+from sklearn.metrics import accuracy_score
+import tools,clf,learn.report,learn
 
 def show_curve(in_path):
     datasets=tools.read_datasets(in_path)
-    acc=clf.cross_acc(datasets)
+    y_true,results,names=get_votes(in_path,datasets)
+    preds=[ learn.voting(results[:i],False) 
+            for i in range(1,len(results))]
+    acc=[accuracy_score(y_true,pred_i) for pred_i in preds]
     print(acc)
-    ordering=np.argsort(acc)
-    ord_data=[datasets[i] for i in ordering]
-    print(ordering)
+
+def get_votes(in_path,datasets):
+    acc=clf.cross_acc(datasets)
+    ordering=np.flip(np.argsort(acc))
+#    ordering.reverse()
     result=learn.report.ens_acc(in_path,clf="LR",acc_only=False)
-    print(result[0][1].shape)
+    y_true,names=result[0][0],result[0][2]
+    results=[result[i] for i in ordering]
+    return y_true,results,names
 
 paths=("../outliners/common/stats/feats","../outliners/ens/sim/feats")
 show_curve(paths)
