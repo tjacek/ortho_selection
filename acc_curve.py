5914aa7aea0170dfaa51b855c144971c47710c5a
tjacek <tjacek@agh.edu.pl>
2020-06-26 20:19:01 +0200

    random ensemble

--- acc_curve.py
+++ acc_curve.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np,random
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 import tools,clf,learn.report,learn
@@ -33,5 +33,18 @@ def show_curve(acc,name="acc_curve",out_path=None):
     plt.clf()
     return acc
 
+def random_ens(in_path,size=None,k=200,clf="LR"):
+    results=learn.report.ens_acc(paths,clf,False)
+    y_true=results[0][0]
+    def helper(size):
+        if(not size):
+            size=random.randrange(len(results)-1)+1
+        subset=random.sample(results, size)
+        pred_i=learn.voting(subset,False)
+        acc_i=accuracy_score(y_true,pred_i)
+        return acc_i
+    acc=np.array([helper(size) for i in range(k)])
+    print("%.4f,%.4f,%.4f" % (np.mean(acc),np.median(acc),np.amax(acc)))
+
 paths=("../outliners/common/stats/feats","../outliners/ens/sim/feats")
-make_curve(paths)
+random_ens(paths)
