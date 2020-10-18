129e39a9bb1b17796457186f9e2dc8aea0ff06e9
tjacek <tjacek@agh.edu.pl>
2020-10-18 04:22:13 +0200

    show_votes

--- audit.py
+++ audit.py
@@ -1,5 +1,5 @@
 import numpy as np
-import ens,learn.report
+import ens,learn.report,tools
 from learn import get_acc
 
 def show_acc(paths):
@@ -13,11 +13,23 @@ def show_acc(paths):
     for acc_i in acc:
         print(acc_i)
 
+def show_votes(paths,binary=True):
+    ensemble=ens.get_ensemble()
+    paths=(paths['common'],paths['ens'])
+    datasets=tools.read_datasets(paths)
+    votes=ens.get_votes(datasets,binary,"LR",None)
+    counter={name_j:np.zeros(20) for name_j in votes[0][2]}
+    for y_true,y_pred,name_i in votes:
+        y_pred=np.argmax(y_pred,axis=1)
+        for y_j,name_j in zip(y_pred,name_i):
+            counter[name_j][y_j]+=1
+    print(counter)
+
 def ens_stats(paths):
     result=learn.report.ens_acc((paths['common'],paths['ens']),clf="LR",acc_only=False)	
     result=[ [result_i[0], np.argmax(result_i[1],axis=1),result_i[2]] for result_i in result]
     full_result=simple_exp(paths)
-    cat_dict=learn.report.cat_by_error(full_result)
+    cat_dict=learn.report.cat_by_error(full_result,binary=True)
     print(learn.report.to_acc(result))
     print(cat_dict)
     for cat_i in cat_dict.keys():
@@ -33,13 +45,12 @@ def simple_exp(paths):
     path=(paths['common'],paths['ens'])
     return ensemble.get_result(path,clf="LR")
 
-common_path="proj/MSR/scale/max_z"
-common_path1="proj/MSR/scale/stats"
+common_path="proj/MSR/scale/max_z/feats"
+common_path1="proj/MSR/scale/stats/feats"
 
 deep_path="proj/MSR/ens/basic/feats"
 paths=get_paths([common_path,common_path1],deep_path)
-print(get_acc((paths['common'],paths['ens'])))
+#print(get_acc((paths['common'],paths['ens'])))
 #show_acc(paths)
-
-
 #ens_stats(paths)
+show_votes(paths)
