988139f7bca9c42c38c5fb793dec81721e79b3ff
tjacek <tjacek@agh.edu.pl>
2020-10-27 22:20:00 +0100

    insert values into db

--- db.py
+++ db.py
@@ -12,8 +12,23 @@ def make_db(in_path,db_path):
 		                            hard integer,
 		                            acc  real)'''
 	conn.execute(sql_str)
+	for i,result_i in enumerate(results):
+		insert(i,result_i.split(","),conn)
+	conn.commit()
+	conn.close()
+
+def insert(i,r_i,conn):
+	hard_i=int(r_i[3]=='True')
+	tuple_i= (i,r_i[0],r_i[1],r_i[2],hard_i,float(r_i[4]))
+	data_i="(%d,'%s','%s','%s',%d,%f)" % tuple_i
+	sql_str="INSERT INTO results VALUES %s" % data_i
+	conn.execute(sql_str)
+
+def show( db_path):
+	conn = sqlite3.connect(db_path)
 	for row in conn.execute("SELECT * FROM results"):
 		print(row)
 
 if __name__ == '__main__':
-    make_db("votes/MSR","db/result.db")
+#    make_db("votes/MSR","db/result.db")
+	show("db/result.db")
