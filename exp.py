d0c2fbe6a8cc35e189bbd9d70e84af28041270c7
tjacek <tjacek@agh.edu.pl>
2020-11-18 13:43:08 +0100

    remove old exp module

--- exp.py
+++ exp.py
@@ -1,118 +1,117 @@
-import os.path#,itertools
-import files,ens,clf,inliners
-import learn.votes,tools
+import re
+import ens,files,inliners
 
-in_dict="proj/MSR"
-out_dict="MSR3"
-common="scale"
+class ExpEnsemble(object):
+	def __init__(self,ensemble,gen=None):
+		if(not gen):
+			gen=GenPaths()
+		self.ensemble=ensemble
+		self.gen=gen
 
-if(common=="mixed"):
-    common_path=["%s/%s" % (in_dict,feat_i) 
-                    for feat_i in ["agum","scale"]]
-else:
-    common_path= "%s/%s" % (in_dict,common) #["MSR_exp/agum","MSR_exp/scale"]
-deep_path="%s/ens/" % in_dict
-out="%s/%s" % (out_dict,common)
-acc=True
-inliner=False 
+	def __call__(self,paths,out,binary=False,clf="LR",acc=True,prefix=""):
+		files.make_dir(out)
+		out="%s/%s" % (out,clf)
+		files.make_dir(out)		
+		for paths_i,out_i in self.gen(paths):
+			out_path="%s/%s" % (out,out_i)
+			acc_i=self.ensemble(paths_i,clf=clf,out_path=out_path,binary=binary,acc_only=acc)
+			result_i=self.format_result(out_path,prefix,acc_i)
+			print(result_i)
 
-class EnsExp(object):
-    def __init__(self,ensemble,prefix=False,common=None,
-                    ens_feats=None):
-        if(not common):
-            common=["stats","basic","sim"]
-        if(not ens_feats):
-            ens_feats=["stats","basic","sim"]
-        self.ensemble=ensemble
-        self.common=common
-        self.ens=ens_feats
-        self.prefix=prefix
+	def format_result(self,out_path,prefix,acc_i):
+		inliner_i=(type(self.ensemble)==inliners.InlinerEnsemble) 
+		prefix_i=out_path.split('/')[-1]
+		feat_desc=prefix_i.replace("_",",")
+		tuple_i=(feat_desc,str(inliner_i),prefix,acc_i)
+		result_i="%s,%s,%s,%s" % tuple_i
+		return result_i
 
-    def __call__(self,paths,out,binary=True,clf_type="LR",acc=True):
-        files.make_dir(out)
-        files.make_dir("%s/%s" % (out,clf_type))
-        print(out)
-        for feat_i in self.common:
-            for feat_j in self.ens:
-                out_ij="%s/%s/%s_%s" % (out,clf_type,feat_i,feat_j)
-                new_paths=get_paths(paths,feat_i,feat_j)
-                clf_ij=get_clf(clf_type)
-                acc_ij=self.ensemble(tuple(new_paths),clf=clf_ij,
-                    out_path=out_ij,binary=binary,acc_only=acc)
-#                acc_ij=self.get_acc(new_paths,clf_ij,out_ij,binary,acc)
-                if(self.prefix):
-                    acc_ij="%s,%s,%s,%s,%s" % (feat_i,feat_j,clf_ij,str(binary),acc_ij)
-                print(acc_ij)
-    
-    def product_exp(self,paths,out,acc=True):
-        args= [[True,False],["LR"]]#,"SVC"]]
-        arg_combs=files.iter_product(args) #list(itertools.product(*args))
-        for binary_i,clf_i in arg_combs:
-            print(clf_i)
-            self(paths,out,binary_i,clf_i,acc=acc)
+	def product_exp(self,paths,out,acc=True):
+		args= [[True,False],["LR","SVC"]]
+		arg_combs=files.iter_product(args) 
+		for binary_i,clf_i in arg_combs:
+			prefix="%s,%s" % (clf_i,str(binary_i))
+			self(paths,out,binary_i,clf_i,acc=acc,prefix=prefix)
 
-def get_paths(old_paths,common,deep):
-    prefix_common,prefix_deep=old_paths
-    deep_path="%s/%s/feats" % (prefix_deep,deep)
-    if(type(common)==tuple):
-        common_path=["%s/%s/feats" % (prefix_common,common_i) 
-                        for common_i in common]
-    else:
-        common_path="%s/%s/feats" % (prefix_common,common)
-    return common_path,deep_path
-#def get_paths(old_paths,common,deep):
-#    prefix_common,prefix_deep=old_paths
-#    deep_path="%s/%s/feats" % (prefix_deep,deep)
-#    if(type(prefix_common)==list):
-#        common_path=["%s/%s/feats" % (common_i,common) 
-#                for common_i in prefix_common]
-#    else:
-#        common_path="%s/%s/feats" % (prefix_common,common)
-#    return common_path,deep_path
+class GenPaths(object):
+	def __init__(self,common=None,binary=None):
+		if(not common):
+			common=["stats","basic","sim"]
+		if(not binary):
+			binary=["stats","basic","sim"]
+		self.common=common
+		self.binary=binary
 
-def get_ensemble(inliner=False,prefix=True,
-                feats_type=False,concat=False):
-    selection=tools.concat_dataset if(concat) else None
-    ensemble= inliners.InlinerEnsemble() if(inliner) else ens.get_ensemble(selection)
-    common,ens_feats=get_feat_types(feats_type)
-    return EnsExp(ensemble,prefix,common,ens_feats)
+	def __call__(self,paths):	
+		old_common,old_deep=paths
+		for feat_i in self.common:
+			for feat_j in self.binary:
+				out_i="%s_%s" % (feat_i,feat_j)
+				common_i=get_paths(common,feat_i)
+				deep_i="%s/%s/feats" % (old_deep,feat_j)
+				new_paths=(common_i,deep_i)
+				yield new_paths,out_i
 
-def get_feat_types(feats_type):
-    if(feats_type=="agum"):
-        return ["stats","basic","sim"],["stats","basic","sim"]
-    if(feats_type=="dtw"):
-        return ["max_z"],["stats","basic","sim"]
-    if(feats_type=="multi"):
-        multi=[["max_z"],["stats","basic","sim"]]
-        multi=files.iter_product(multi)
-        return multi,["stats","basic"]
-    return None,None
+def get_paths(common,feat):
+	if(type(feat)==list):
+		common=[ "%s/%s/feats" % (common,feat_i)
+				for feat_i in feat]
+	else:
+		common="%s/%s/feats" % (common,feat)
+	return common
 
-def get_clf(raw_clf):
-    if(raw_clf=="mixed"):
-        return ["LR","SVC"]
-    return raw_clf                
-
-def unify_votes(paths,out_path):
-    paths=[files.top_files(path_i) 
-            for path_i in paths ]
-    paths=list(map(list, zip(*paths)))
-    for path0,path1 in paths:
-        out_i="%s/%s" % (out_path,path0.split("/")[-1])
-        learn.votes.unify_votes([path0,path1],out_i)
+def get_exp_ens(inliner=False,gen=None):
+	ensemble= inliners.InlinerEnsemble() if(inliner) else ens.get_ensemble()
+	if(gen=="no_sim"):
+		feats=["stats","basic"]
+		gen=GenPaths(feats,feats)
+	elif(gen=="dtw"):
+		common=[["stats","maxz"],["basic","maxz"],
+				["sim","maxz"]]
+		binary=["stats","basic","sim"]
+		gen=GenPaths(common,binary)
+	return ExpEnsemble(ensemble,gen)
 
 def show_result(in_path,acc=True):
     paths=files.bottom_dict(in_path)
+    result=[]
     for path_i in paths:
         for binary_i in [True,False]:
             acc_i=ens.read_result(path_i,binary_i,acc)
-            print("%s,%s,%s" % (path_i,binary_i,acc_i))
+            prefix_i=path_i.split('/')[-1]
+            clf_i=path_i.split('/')[-2]
+            prefix_i=format_prefix(prefix_i)
+            tuple_i=(prefix_i,clf_i,binary_i,acc_i)
+            str_i="%s,%s,%s,%s" % tuple_i
+            print(str_i)
+            result.append(str_i)
+    return result
 
-paths=["MHAD/agum/SVC","MHAD/scale/SVC"]
-out_path="MHAD/mixed/SVC"
+def format_prefix(prefix_i):
+	bracket_re=re.compile('\\[(.*?)\\]')
+	if(bracket_re.match(prefix_i)):
+		head,rest=prefix_i.split("]")
+		head= re.sub(r"(\s+)|(\[)|\'","",head)
+		head=head.replace(",","#")
+		rest=rest.replace("_","")
+		prefix_i="%s,%s" % (head,rest)
+	return prefix_i.replace("_",",")
 
-show_result("MSR3/scale",True)
-#unify_votes(paths,out_path)
+def get_common_feats(dataset,feat_type):
+    if(feat_type=="mixed"):
+        common=["exp/%s/%s" % (dataset,str_i) 
+                for str_i in ["simple","agum"]]
+    else:
+        common="exp/%s/%s" % (dataset,feat_type)
+    return common
 
-ensemble=get_ensemble(inliner,feats_type="multi",concat=False)
-ensemble.product_exp((common_path,deep_path),out,acc=acc)
+if __name__ == '__main__':
+#    dataset="proj"
+#    feat_type="max_z3"
+    out_path="votes/MSR2"#"%s/%s" % (dataset,feat_type)
+    inliner=False
+    common="MSR_exp/scale1"
+    deep_path="MSR_exp/ens"
+    ens_exp=get_exp_ens(inliner=inliner,gen="dtw")
+    ens_exp.product_exp((common,deep_path),out_path,acc=True)
+#    show_result(out_path,acc=True)
