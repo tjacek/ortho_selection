c9f462e90f5bad0118bfcb11fb3b88ddc56767d3
tjacek <tjacek@agh.edu.pl>
2020-11-20 23:57:32 +0100

    curve

--- acc_curve.py
+++ acc_curve.py
@@ -1,24 +1,17 @@
 import numpy as np,random
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
-import tools,clf,learn.report,learn
+#import tools,clf,learn.report,learn
+import ens,learn
 
-def make_curve(in_path):
-    y_true,results,names=get_votes(in_path)
-    preds=[ learn.voting(results[:i],False) 
-            for i in range(1,len(results))]
+def make_curve(in_path,binary=False):
+    votes=ens.get_votes(None,None,None,in_path)
+    preds=[ learn.voting([vote_i],binary) 
+            for vote_i in votes]
+    y_true=votes[0][0]
     acc=[accuracy_score(y_true,pred_i) for pred_i in preds]
     print(acc)
-    show_curve(acc)
-
-def get_votes(in_path):
-    datasets=tools.read_datasets(in_path)
-    acc=clf.cross_acc(datasets)
-    ordering=np.flip(np.argsort(acc))
-    result=learn.report.ens_acc(in_path,clf="LR",acc_only=False)
-    y_true,names=result[0][0],result[0][2]
-    results=[result[i] for i in ordering]
-    return y_true,results,names
+#    show_curve(acc)
 
 def show_curve(acc,name="acc_curve",out_path=None):
     plt.title(name)
@@ -47,4 +40,5 @@ def random_ens(in_path,size=None,k=200,clf="LR"):
     print("%.4f,%.4f,%.4f" % (np.mean(acc),np.median(acc),np.amax(acc)))
 
 paths=("../outliners/common/stats/feats","../outliners/ens/sim/feats")
-random_ens(paths)
+#random_ens(paths)
+make_curve("votes/third")
